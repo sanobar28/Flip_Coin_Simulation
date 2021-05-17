da8287f9fb7c88ccb68b955ec32a42de35b272cf
sanobar28 <sanobar.mujawar@gmail.com>
2021-05-17 19:07:58 +0530

    [Sanobar] added no. of times heads or tails win

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 
-flip=$(( RANDOM%2 ))
-if [ $flip -eq 1 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+heads=0
+tails=0
+
+for (( count=0; count<50; count++ ))
+do
+	flip=$(( RANDOM%2 ))
+	if [ $flip -eq 1 ]
+	then
+		heads=$(( $heads+1 ))
+	else
+		tails=$(( $tails+1 ))
+	fi
+done
+
+echo "Heads wins $heads number of times"
+echo "Tails wins $tails number of times"
